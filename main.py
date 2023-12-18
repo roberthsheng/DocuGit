5b79a9b52711baafe5ff35126497bc2e64e49e5d
roberthsheng <roberthsheng@gmail.com>
2023-12-18 15:20:01 -0500

    writing response

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ def read_file():
         commits = data.split('commit')
         commits.reverse() # reversing so the first commit is first in the list
         for commit in commits:
-            # use openai to summarize the commit, idk if this code works yet tbh
+            # prompt openai to summarize the commit message, no clue if this works or not this is just boilerplate
             response = openai.Completion.create(
                 engine="davinci",
                 prompt=commit,
-                temperature=0.3,
+                temperature=0.7,
                 max_tokens=60,
                 top_p=1,
                 frequency_penalty=0.5,
                 presence_penalty=0.5,
                 stop=["\n", "commit"]
             )
+            # write resposne to file
+            with open('commits.txt', 'a') as f:
+                f.write(response)
+
         return commits
 
 read_file()
